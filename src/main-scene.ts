11fac49c4ffab7ef15d1447f4bd3a81e2c213d10
Jatheepan Kanthavel <theepan@cutedrops.com>
2023-04-01 13:33:10 -0400

    new images

--- src/main-scene.ts
+++ src/main-scene.ts
@@ -1,45 +1,20 @@
 import * as Phaser from 'phaser';
 import { Types } from 'phaser';
-import marioBigRunning from './images/mario-big-running.png';
-import skyBackground from './images/asset/background/Day/1.png';
-import buildingBackground from './images/asset/background/Day/2.png';
-import treeBackground from './images/asset/background/Day/5.png';
 import config from './config';
 
 export default class MainScene extends Phaser.Scene {
   player: Types.Physics.Arcade.SpriteWithDynamicBody | null = null;
 
   preload() {
-    this.load.image('sky', skyBackground);
-    this.load.image('building', buildingBackground);
-    this.load.image('tree', treeBackground);
-    this.load.spritesheet('mario-big-running', marioBigRunning, {
-      frameWidth: 16,
-      frameHeight: 30,
-    });
-
+    this.load.tilemapTiledJSON('map', 'untitled.json');
     console.log('this is preload stage');
   }
 
   create() {
-    this.add
-      .tileSprite(0, 0, 576 * 2, 324, 'building')
-      .setScale(1)
-      .setOrigin(0, 0);
-    this.add.tileSprite(0, 0, 576 * 2, 400, 'tree').setOrigin(0, 0);
-
-    this.anims.create({
-      key: 'mario-left',
-      frames: this.anims.generateFrameNumbers('mario-big-running', {
-        start: 0,
-        end: 3,
-      }),
-      frameRate: 10,
-      repeat: -1,
-    });
-
-    // this.player = this.physics.add.sprite(16, 30, 'mario-big-running');
-    // this.player.setCollideWorldBounds(true);
-    // this.player.anims.play('mario-left', true);
+    //
+    const map = this.make.tilemap({ key: 'map' });
+    const tileset = map.addTilesetImage('untitled', 'tiles');
+    const backgroundKey = map.addTilesetImage('background', tileset, 0, 0);
+    // const layer = map.createStaticLayer('Tile Layer 1', tileset, 0, 0);
   }
 }
